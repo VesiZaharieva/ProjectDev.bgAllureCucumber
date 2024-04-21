b766d4fdc62b77f6c604ec5e676960f45cb2f2b8
VesiZaharieva <vesela.zaharieva@abv.bg>
2024-04-21 13:21:39 +0300

    Project version with Allure and Cucumber

--- src/test/java/selenium/base/BasePage.java
+++ src/test/java/selenium/base/BasePage.java
@@ -1,14 +1,13 @@
 package selenium.base;
 
 import dev.selenium.driver.DriverFactory;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 
